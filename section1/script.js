0277cc55af7447cac079bb02f432261e7643c7e2
bluejet80 <know_jarett@yahoo.com>
2022-01-08 11:44:22 -0600

    Moving on to Section2

--- section1/script.js
+++ section1/script.js
@@ -1,4 +1,87 @@
-let js = 'amazing';
-if (js === 'amazing') alert('Javascript is Fun!')
+/* 
+const firstName = "Jarett"
+const job = "Manager"
+const birthYear = 1980;
+const year = 2037
 
-console.log(40 + 8 - 200 + 20);
+const jarett = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+console.log(jarett);
+
+const jarettNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
+console.log(jarettNew);
+
+*/
+/*
+const age = 16;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+    console.log(`Sarah can start drivers License.`)
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is not old enough.
+    Wait another ${yearsLeft} years.`)
+}
+
+const birthYear = 1980;
+
+let century;
+if (birthYear <= 2000) {
+    century = '20th';
+} else {
+    century = '21st';
+}
+
+console.log(`You were born in the ${century} century.`)
+*/
+/*
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+
+
+// if (hasDriversLicense && hasGoodVision) {
+//     console.log(`Sarah is able to drive.`)
+// } else {
+//     console.log(`Someone else should drive...`)
+// }
+
+const isTired = false; // c
+
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log(`Sarah is able to drive.`)
+} else {
+    console.log(`Someone else should drive...`)
+}
+
+*/
+/*
+const day = 'hamburger';
+
+switch (day) {
+    case 'monday':
+        console.log(`Plan my course structure`);
+        console.log(`Go to coding meetup.`);
+        break;
+    case 'tuesday':
+        console.log(`Eat a Sandwhich`);
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log(`Write code examples`);
+        break;
+    case 'friday':
+        console.log(`Record Videos`);
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log(`Enjoy the weekend :D`);
+        break;
+    default:
+        console.log(`Not a valid Day of the week.`);
+}
+*/
+
---
+++ section2/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(`I can drive!`)
+*/
+
+function logger() {
+    console.log('My name is Jarett');
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(3, 4);
+console.log(appleJuice);
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1980);
+console.log(age1);
+
+
+// Function Expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991)
+console.log(age2);
