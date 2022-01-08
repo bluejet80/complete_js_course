224cda0ae1d84de97c1afdda4f78d31e2a387b6a
bluejet80 <know_jarett@yahoo.com>
2022-01-08 16:41:58 -0600

    Update for wiki

--- section2/script.js
+++ section2/script.js
@@ -44,3 +44,14 @@ const calcAge3 = birthYear => 2037 - birthYear
 
 const age3 = calcAge3(1930);
 console.log(age3);
+
+// new function
+
+const yearUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years.`
+}
+
+console.log(yearUntilRetirement(1991, 'Jimmy'));
+
