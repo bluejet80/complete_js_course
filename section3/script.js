7e3b81a76e59fd88c3a5b347330e9f2d1dd0efa3
bluejet80 <know_jarett@yahoo.com>
2022-01-09 21:19:10 -0600

    starting section4

--- section3/script.js
+++ section3/script.js
@@ -1,7 +1,37 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 23;
-if (x === 23) console.log('Happy');
+// project 1
+/*
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
-const calcAge = birthYear => 2037 - birthYear;
+const temperatures2 = [5, -7, -4, -2, 'error', 5, 10, 18, 19, 'error', 11, 8];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const mergeArray = function (array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    array2.push(array1[i]);
+  }
+  console.log(array2);
+  return calcTempAmplitude(array2);
+};
+
+console.log(mergeArray(temperatures, temperatures2));
+
+// could have also used:
+// const array3 = array1.concat(array2);
+*/
