d8ec97223d363e7cc33942b25da8f3422bdb863e
bluejet80 <know_jarett@yahoo.com>
2022-01-10 22:46:07 -0600

    click events update

--- section4_guess/script.js
+++ section4_guess/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'You are a Jackass!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 300;
+document.querySelector('.guess').value = 34;
+console.log(document.querySelector('.guess').value);
