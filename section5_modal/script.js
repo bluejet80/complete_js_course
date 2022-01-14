6e106cda3b422cb0978c17813111604e5c4cdb9e
bluejet80 <know_jarett@yahoo.com>
2022-01-13 22:35:51 -0600

    halfway through section 5

--- section5_modal/script.js
+++ section5_modal/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    switch (i) {
+      case 0:
+        document.querySelector('.page1').classList.remove('hidden');
+        overlay.classList.remove('hidden');
+        break;
+      case 1:
+        document.querySelector('.page2').classList.remove('hidden');
+        overlay.classList.remove('hidden');
+        break;
+      case 2:
+        document.querySelector('.page3').classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    }
+  });
