0715a438ed22391066e7950fa4673da3d1143285
bluejet80 <know_jarett@yahoo.com>
2022-01-13 18:32:04 -0600

    trying to merge from linux box

--- section4_guess/script.js
+++ section4_guess/script.js
@@ -1,11 +1,15 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'You are a Jackass!';
-console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'You are a Jackass!';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 300;
-document.querySelector('.guess').value = 34;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 300;
+// document.querySelector('.guess').value = 34;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
