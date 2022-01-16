b468876432ce865638b4bb6a13241334d706409c
bluejet80 <know_jarett@yahoo.com>
2022-01-16 12:49:52 -0600

    Working on varoious Array Methods

--- section7_behind_scenes/script.js
+++ section7_behind_scenes/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+// Array Methods
+
+// Map Method
+
+const wheel = [5, 2, 6, 9];
+const result = wheel.map(item => {
+  return item * 2;
+});
+
+console.log(result);
+
+const tarry = ['sweet', 'hash', 'dirty', 'salty'];
+const pickle = tarry.map(a => {
+  return a + 'pickle';
+});
+
+console.log(pickle);
+
+// forEach Method
+
+const items = [5, 2, 6, 1];
+const newArray = [];
+items.forEach(item => {
+  newArray.push(item * 34);
+});
+console.log(newArray);
+
+// filter Method
+let toe;
+toe = 6;
+const pizza = [2 * toe, 3 / toe, toe, 10 * toe];
+
+console.log(pizza);
+
+console.log(Math.floor(19.34));
