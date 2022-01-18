63ee550a439bbc7c0483888cfd5a250244b449ca
bluejet80 <know_jarett@yahoo.com>
2022-01-17 21:32:26 -0600

    finished the functions lecture, learned about argument keyword

--- section7_behind_scenes/script.js
+++ section7_behind_scenes/script.js
@@ -16,14 +16,38 @@ const mary = {
     console.log(this);
     console.log(2037 - this.year);
 
-    const self = this;
-    const isMillenial = function () {
-      console.log(self);
-      console.log(self.year >= 1981 && self.year <= 1996);
-      // console.log(this.year >= 1981 && this.year <= 1996);
+    // const self = this;  // pre-ES6 version
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    //   // console.log(this.year >= 1981 && this.year <= 1996);
+    // };
+    // isMillenial();
+
+    const isMillenial = () => {
+      // post ES6 version
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
     };
+
     isMillenial();
   },
 };
 
 mary.calcAge();
+
+// arguments keyword
+
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+console.log(addExpr(2, 5));
+console.log(addExpr(3, 5, 7, 4)); // only adds the first two
+
+var addArrow = (a, b) => {
+  //console.log(arguments); // not defined in arrow functions
+  return (a = b);
+};
+
+addArrow(4, 5, 6);
