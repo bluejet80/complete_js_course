fe74fe93b11aa7c180599cd30f2d42a632266044
bluejet80 <know_jarett@yahoo.com>
2022-01-17 20:33:04 -0600

    figured out how to get the value and the key of an object

--- section7_behind_scenes/script.js
+++ section7_behind_scenes/script.js
@@ -6,6 +6,22 @@ const jarett = {
   birthYear: 1980,
   firstName: 'Jarett',
   lastName: 'Young',
+  jobTypes: [
+    'astronaut',
+    'farmer',
+    'baker',
+    'scientist',
+    'programmer',
+    'sociologist',
+    'educator',
+    'clerk at the liquor store',
+    'ceramic artist',
+    'bookbinder',
+  ],
+  jobDescript: {
+    baker: 'bakes bread',
+    scientist: 'runs tests',
+  },
   printName: function () {
     return this.firstName + ' ' + this.lastName;
   },
@@ -17,8 +33,32 @@ const jarett = {
   printJournalNumber: function () {
     return `${this.printName()} has ${this.journalNumber} Journals.`;
   },
+  printMessage: function (a) {
+    if (a <= this.jobTypes.length) {
+      let i = 0;
+      while (i < a) {
+        console.log(`I am a ${this.jobTypes[i]}`);
+        i++;
+      }
+    } else {
+      console.log('to many numbers...');
+    }
+  },
+  printJobDescript: function () {
+    let i = 0;
+    const length = Object.keys(this.jobDescript).length;
+
+    while (i < length) {
+      const work = jarett.jobDescript[Object.keys(jarett.jobDescript)[i]];
+      const job = Object.keys(jarett.jobDescript)[i];
+      console.log(`I am a ${job} and I ${work}`);
+      i++;
+    }
+  },
 };
 
 console.log(jarett.printName());
 console.log(jarett.printAge());
 console.log(jarett.printJournalNumber());
+jarett.printMessage(10);
+jarett.printJobDescript();
