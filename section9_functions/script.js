340f5ef4da8ff0737b7faec0e96418c0ea8860ff
bluejet80 <know_jarett@yahoo.com>
2022-02-19 21:46:43 -0600

    higher level functions

--- section9_functions/script.js
+++ section9_functions/script.js
@@ -1,5 +1,53 @@
 'use strict';
 
+/// having Funcitons return other functions
+
+// higher order functions, those functions that takes functions as arguments
+
+const oneWord = function (str) {
+  //callback function
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  //callback function
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  // higher order function takes a callback function as a parameter
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`); // retunrs the name of the function
+};
+
+transformer('javascript is the best!', upperFirstWord);
+
+transformer('javascript is the best!', oneWord);
+
+const adder = function (...num) {
+  //function takes an arbitrary number of arguments
+  console.log(num);
+  let result = 0;
+  for (const item of num) {
+    result += item;
+  }
+  console.log(`${num.join(' + ')} = ${result}`);
+};
+
+adder(23, 45, 27, 88);
+
+// another example
+const high5 = function () {
+  console.log("What's up man!");
+};
+
+document.body.addEventListener('click', high5); //runs high5 when body is clicked
+// high5 is a callback function
+
+['jarett', 'harvey', 'peter'].forEach(high5);
+
 // //  Passing Arguments: Value vs. Reference
 
 // const flight = 'LH234';
@@ -27,8 +75,9 @@
 //   person.passport = Math.trunc(Math.random() * 100000000000);
 // };
 
-// newPassport(jarett);
+// //newPassport(jarett);
 // checkIn(flight, jarett);
+// console.log(flight);
 // console.log(jarett);
 
 // ///// --------- Default values
@@ -49,7 +98,7 @@
 //   };
 
 //   console.log(booking);
-//   bookings.push(booking);
+//   bookings.push(booking); // pushes data in oject booking to the array bookings
 // };
 
 // createBooking('LH123');
