2bbdaa708601565ea005e0e0d7b45b389fda9e6b
bluejet80 <know_jarett@yahoo.com>
2022-07-01 22:29:40 -0500

    finished the classes

--- section14_OOP/README.md
+++ section14_OOP/README.md
@@ -13,3 +13,30 @@ Getters and Setters are functions that Get and Set a value
 ## Object.create
 
 in the file create.js it shows how to link objects together in a prototype chain using only Object.create.
+
+## Class Fields
+
+1. Public Fields
+2. Private Fields
+3. Public Methods
+4. Private Methods
+
+### Public Fields
+
+Public fields are on the instances and not on the prototypes
+They are also referencable by the this keyword
+
+### Private Fields
+
+Private fields are created by putting a hashtag # infront of the property/field.
+Thn whenever they are referenced within the class, you just reference them with the #name.
+If you try to access a # private property outside of the class then you get a syntax error.
+
+Private properties are initialized above the constructor function and then included in the constructor function.
+
+### Public Methods
+
+These are just the same as you have always done. And all these methods are part of the public interface(API) and they are
+attached to the prototype
+
+### Private Methods
--- section14_OOP/classes1.js
+++ section14_OOP/classes1.js
@@ -1,1 +1,58 @@
 "use strict";
+
+class Account {
+  //public fields (instances)
+  locale = navigator.language;
+
+  // Private fields (instances)
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    // Protected
+    this.#pin = pin;
+    //this._movements = [];
+    //this.local = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  //Public interface
+
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+    return this; // returning this will make the method chainable
+  }
+  withdrawl(val) {
+    this.deposit(-val);
+    return this;
+  }
+
+  _approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan Approved!`);
+    }
+  }
+}
+
+const acct1 = new Account("Jarett", "dollars", 1234);
+
+acct1.deposit(1000);
+acct1.withdrawl(2.75);
+acct1.withdrawl(56.3);
+acct1.requestLoan(1000);
+
+console.log(acct1.getMovements());
+
+console.log(acct1);
